22147851a4369d07debac88a24790a9193025617
Anwar bloch <anwardev500@gmail.com>
2026-03-11 17:50:09 +0500

    Dashboard page updated

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,50 @@
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
+    const [tasks, setTasks] = useState([]);
+    const [assignments, setAssignments] = useState([]);
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    useEffect(() => {
+        const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        const storedAssignments =
+            JSON.parse(localStorage.getItem("assignments")) || [];
+
+        setTasks(storedTasks);
+        setAssignments(storedAssignments);
+    }, []);
+
+    const completed = tasks.filter(t => t.status === "Completed").length;
+    const pending = tasks.filter(t => t.status === "Pending").length;
+
     return (
         <>
             <Navbar />
-            <div style={{ padding: "40px" }}>
-                <h1>Dashboard</h1>
-                <p>Welcome to Student Task Manager</p>
+            <div className="dashboard">
+                <h1>Welcome, {user?.name} 👋</h1>
+
+                <div className="cards">
+                    <div className="card">
+                        <h3>Total Tasks</h3>
+                        <p>{tasks.length}</p>
+                    </div>
+
+                    <div className="card">
+                        <h3>Completed Tasks</h3>
+                        <p>{completed}</p>
+                    </div>
+
+                    <div className="card">
+                        <h3>Pending Tasks</h3>
+                        <p>{pending}</p>
+                    </div>
+
+                    <div className="card">
+                        <h3>Total Assignments</h3>
+                        <p>{assignments.length}</p>
+                    </div>
+                </div>
             </div>
         </>
     );
