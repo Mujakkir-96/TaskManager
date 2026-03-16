a10863ce51b9f40cfba0076942f6f3e0ddf0134e
Anwar bloch <anwardev500@gmail.com>
2026-03-16 22:39:00 +0500

    Dark and Light mode toggle added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,22 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
 
 function Navbar() {
     const navigate = useNavigate();
     const user = localStorage.getItem("user");
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") || "light"
+    );
+    useEffect(() => {
+        document.body.className = theme;
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    };
 
     const logout = () => {
         localStorage.removeItem("user");
@@ -14,12 +28,20 @@ function Navbar() {
     return (
         <div className="navbar">
             <h2>Student Manager</h2>
-            <div>
+            <div className="items">
                 <Link to="/dashboard">Dashboard</Link>
                 <Link to="/tasks">Tasks</Link>
                 <Link to="/add-task">Add Task</Link>
                 <Link to="/assignments">Assignments</Link>
-                <button onClick={logout}>Logout</button>
+
+                <div>
+
+                    <button className="theme-btn" onClick={toggleTheme}>
+                        {theme === "light" ? <FaMoon /> : <FaSun />}
+                    </button>
+
+                    <button onClick={logout}>Logout</button>
+                </div>
             </div>
         </div>
     );
